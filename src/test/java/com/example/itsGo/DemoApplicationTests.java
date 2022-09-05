641d9da2470789d63d501ae85e52b17e60059966
HJKim <kjh2919@gmail.com>
2022-09-05 18:36:11 +0900

    dependencies

---
+++ src/test/java/com/example/itsGo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itsGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
